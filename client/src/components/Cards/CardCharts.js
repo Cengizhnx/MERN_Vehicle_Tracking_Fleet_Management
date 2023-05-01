b675f52dc6c131f2ed76dfbb0d3b861234c3073e
Cengiz <cengiz69mg@gmail.com>
2023-05-01 21:31:33 +0300

    header responsive completed

--- client/src/components/Cards/CardCharts.js
+++ client/src/components/Cards/CardCharts.js
@@ -4,7 +4,7 @@ import { FleetsChart } from "./FleetsChart";
 
 function CardCharts() {
   return (
-    <div className="flex flex-row space-x-5">
+    <div className="flex flex-col items-center justify-content md:flex-row md:space-x-5">
       <div className="flex sm:flex-row flex-col space-y-5 sm:space-y-0 sm:space-x-3 mt-5 flex-row w-1/2 p-5 rounded bg-white items-center justify-around">
         <RoutesChart></RoutesChart>
       </div>
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../redux/userSlice";
 
 function Header() {
   const [open, setOpen] = useState(false);
+  const [openNav, setOpenNav] = useState(false);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -14,6 +15,8 @@ function Header() {
     dispatch(logout());
     navigate("/login");
   };
+
+  const user = useSelector((state) => state.users.user);
 
   return (
     <div>
@@ -63,82 +66,110 @@ function Header() {
       </header>
 
       <header
-        x-data="{ isOpen: false }"
+        // x-data="{ isOpen: false }"
         className="w-full bg-sidebar py-5 px-6 sm:hidden"
       >
         <div className="flex items-center justify-between">
           <a
-            href="index.html"
+            href="/"
             className="text-white text-3xl font-semibold uppercase hover:text-gray-300"
           >
             Admin
           </a>
           <button
+            onClick={(prev) => setOpenNav((prevState) => !prevState)}
             // onClick="isOpen = !isOpen"
             className="text-white text-3xl focus:outline-none"
           >
-            <i x-show="!isOpen" className="fas fa-bars"></i>
-            <i x-show="isOpen" className="fas fa-times"></i>
+            {!openNav ? (
+              <i className="fas fa-bars"></i>
+            ) : (
+              <i className="fas fa-times"></i>
+            )}
           </button>
         </div>
 
-        <nav className="isOpen ? 'flex': 'hidden' flex flex-col pt-4">
-          <a
-            href="index.html"
-            className="flex items-center active-nav-link text-white py-2 pl-4 nav-item"
+        <nav className={`flex flex-col pt-4 ${openNav ? "block" : "hidden"}`}>
+          <Link
+            className={`flex items-center text-white py-2 pl-4 nav-item ${
+              window.location.pathname === "/" && "active-nav-link opacity-100"
+            }`}
+            to="/"
           >
-            <i className="fas fa-tachometer-alt mr-3"></i>
-            Dashboard
-          </a>
-          <a
-            href="blank.html"
-            className="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item"
+            <i className="fas fa-home mr-3"></i>
+            Ana Sayfa
+          </Link>
+
+          {user.name === "admin" && (
+            <>
+              <Link
+                className={`flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item ${
+                  window.location.pathname === "/add-customer" &&
+                  "active-nav-link opacity-100"
+                }`}
+                to="/add-customer"
+              >
+                <i className="fas fa-user-plus mr-3"></i>
+                Müşteri Ekle
+              </Link>
+              <Link
+                className={`flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item ${
+                  window.location.pathname === "/customers" &&
+                  "active-nav-link opacity-100"
+                }`}
+                to="/customers"
+              >
+                <i className="fas fa-users mr-3"></i>
+                Müşteriler
+              </Link>
+              <Link
+                className={`flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item ${
+                  window.location.pathname === "/add-fleet" &&
+                  "active-nav-link opacity-100"
+                }`}
+                to="/add-fleet"
+              >
+                <i className="fas fa-table mr-3"></i>
+                Filo Ekle
+              </Link>
+            </>
+          )}
+
+          <Link
+            className={`flex items-center text-white py-2 pl-4 nav-item ${
+              window.location.pathname === "/fleets" &&
+              "active-nav-link opacity-100"
+            }`}
+            to="/fleets"
           >
-            <i className="fas fa-sticky-note mr-3"></i>
-            Blank Page
-          </a>
-          <a
-            href="tables.html"
-            className="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item"
+            <i className="fas fa-building mr-3"></i>
+            Filolar
+          </Link>
+
+          {user.name !== "admin" && (
+            <Link
+              className={`flex items-center text-white py-2 pl-4 nav-item ${
+                window.location.pathname === "/add-route" &&
+                "active-nav-link opacity-100"
+              }`}
+              to="/add-route"
+            >
+              <i className="fas fa-route mr-3"></i>
+              Rota Ekle
+            </Link>
+          )}
+
+          <Link
+            className={`flex items-center text-white py-2 pl-4 nav-item ${
+              window.location.pathname === "/routes" &&
+              "active-nav-link opacity-100"
+            }`}
+            to="/routes"
           >
-            <i className="fas fa-table mr-3"></i>
-            Tables
-          </a>
-          <a
-            href="forms.html"
-            className="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item"
-          >
-            <i className="fas fa-align-left mr-3"></i>
-            Forms
-          </a>
-          <a
-            href="tabs.html"
-            className="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item"
-          >
-            <i className="fas fa-tablet-alt mr-3"></i>
-            Tabbed Content
-          </a>
-          <a
-            href="calendar.html"
-            className="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item"
-          >
-            <i className="fas fa-calendar mr-3"></i>
-            Calendar
-          </a>
-          <a
-            href="#"
-            className="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item"
-          >
-            <i className="fas fa-cogs mr-3"></i>
-            Support
-          </a>
-          <a
-            href="#"
-            className="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item"
-          >
-            <i className="fas fa-user mr-3"></i>
-            My Account
-          </a>
+            <i className="fas fa-road mr-3"></i>
+            Rotalar
+          </Link>
+
           <a
             href="#"
             className="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item"
@@ -146,13 +177,13 @@ function Header() {
             <i className="fas fa-sign-out-alt mr-3"></i>
             Sign Out
           </a>
-          <button className="w-full bg-white cta-btn font-semibold py-2 mt-3 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-300 flex items-center justify-center">
+          {/* <button className="w-full bg-white cta-btn font-semibold py-2 mt-3 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-300 flex items-center justify-center">
             <i className="fas fa-arrow-circle-up mr-3"></i> Upgrade to Pro!
-          </button>
+          </button> */}
         </nav>
-        <button className="w-full bg-white cta-btn font-semibold py-2 mt-5 rounded-br-lg rounded-bl-lg rounded-tr-lg shadow-lg hover:shadow-xl hover:bg-gray-300 flex items-center justify-center">
+        {/* <button className="w-full bg-white cta-btn font-semibold py-2 mt-5 rounded-br-lg rounded-bl-lg rounded-tr-lg shadow-lg hover:shadow-xl hover:bg-gray-300 flex items-center justify-center">
           <i className="fas fa-plus mr-3"></i> New Report
-        </button>
+        </button> */}
       </header>
     </div>
   );
